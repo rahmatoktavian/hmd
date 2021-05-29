6378ac47eb003b9f0920a1aec16409aa1e294158
rahmatoktavian <rahmatoktavian@gmail.com>
2021-05-29 09:15:48 +0700

    initapp

--- application/views/theme/sidebar.php
+++ application/views/theme/sidebar.php
@@ -13,47 +13,28 @@
 
   <!-- Nav Item - Charts -->
   <li class="nav-item active">
-    <a class="nav-link" href="<?php echo site_url('provinsi/read');?>">
-      <i class="fas fa-fw fa-users"></i>
-      <span>Provinsi</span></a>
-  </li>
-
-  <!-- Nav Item - Charts -->
-  <li class="nav-item">
-    <a class="nav-link" href="<?php echo site_url('kota/read');?>">
-      <i class="fas fa-fw fa-home"></i>
-      <span>Kota</span></a>
+    <a class="nav-link" href="<?php echo site_url('anggota/read');?>">
+      <i class="fas fa-fw fa-user"></i>
+      <span>Anggota</span>
+    </a>
   </li>
 
   <!-- Heading -->
   <div class="sidebar-heading">
-    Addons
+    Peminjaman
   </div>
 
-  <!-- Nav Item - Pages Collapse Menu -->
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-      <i class="fas fa-fw fa-folder"></i>
-      <span>Pages</span>
+    <a class="nav-link" href="<?php echo site_url('peminjaman/read');?>">
+      <i class="fas fa-fw fa-paperclip"></i>
+      <span>Peminjaman</span>
     </a>
-    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Login Screens:</h6>
-        <a class="collapse-item" href="login.html">Login</a>
-        <a class="collapse-item" href="register.html">Register</a>
-        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-        <div class="collapse-divider"></div>
-        <h6 class="collapse-header">Other Pages:</h6>
-        <a class="collapse-item" href="404.html">404 Page</a>
-        <a class="collapse-item" href="blank.html">Blank Page</a>
-      </div>
-    </div>
   </li>
-
   <li class="nav-item">
-    <a class="nav-link" href="#">
-      <i class="fas fa-fw fa-chart-area"></i>
-      <span>Charts</span></a>
+    <a class="nav-link" href="<?php echo site_url('buku/read_theme');?>">
+      <i class="fas fa-fw fa-book"></i>
+      <span>Buku</span>
+    </a>
   </li>
 
   <hr class="sidebar-divider d-none d-md-block">
